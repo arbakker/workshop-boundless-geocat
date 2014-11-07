3a0d159b4103c97c84302dbb9b56c21e901ac618
Anton Bakker <anton.bakker@geocat.net>
2014-11-07 12:50:45 +0100

    now with correct attributes int  the dropdown

--- data/extract-descr.py
+++ data/extract-descr.py
@@ -3,24 +3,23 @@ import re
 
 fields = open('census_nl-fields.txt', 'r')
 count=0
-
+count_match=0
 output= open("dictionary.txt", 'w+')
-
-
 for line in fields:
     count+=1
-    count_pg=0
     match=re.match("^.*: .*$", line, flags=0)
     if match:
-
+        count_match+=1
         split_dict = line.split()
 
-        postgres_columns = open('column_names.txt', 'r')
-        for pg_line in postgres_columns:
-            count_pg+=0
-            split_postgres = pg_line.split()
-            if split_postgres[0].upper() == split_dict[0].upper()[:-1]:
-                output.write(line)
-        postgres_columns.close()
+        exclude = ["GM_CODE","GM_NAAM","WK_CODE","WK_NAAM"]
+        if  split_dict[0].upper()[:-1] not in exclude:
+            postgres_columns = open('column_names.txt', 'r')
+            for pg_line in postgres_columns:
+                split_postgres = pg_line.split()
+
+                if split_postgres[0].upper() == split_dict[0].upper()[:-1]:
+                    output.write(line)
+            postgres_columns.close()
 fields.close()
 output.close()
